97f10468c9fb3369a4f27865f520c4ea40159824
israr-faridi <vbmovielover465@gmail.com>
2024-02-17 00:59:40 +0500

    Day no 1 let var cont concept

---
+++ Day no 1/Let_Var_Const.js
@@ -0,0 +1,13 @@
+const accountId = 1445533;
+let accountEmail="Babo@example.com";
+var accountPassword="151000";
+accountCity="Karachi";
+let accountState;
+
+// accountId=999; // not Allowed
+
+accountEmail="Nana@gmail.com"
+accountPassword="6699"
+accountCity="Lahore"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
