05443b2a7085b24ca66d289a4f467b07cf0ba54e
israr-faridi <vbmovielover465@gmail.com>
2024-03-14 09:41:27 +0500

    num

---
+++ Day_no_4/stack_and_heap.js
@@ -0,0 +1,25 @@
+// // stack
+// let nameStact = "first";
+// let nameStactTest = nameStact;
+
+// nameStactTest = "second";
+// console.log(nameStactTest);
+///// In heap data copy and then you change copy so only change copy not data
+
+
+
+// HEAP 
+let heapExample = {
+    name: 'zora',
+    age: 44,
+    gmail: "zora@gmail.com",
+}
+
+let heapTest = heapExample;
+console.log(heapExample);
+console.log(heapTest);
+
+heapTest.gmail="naheem@gmail.com";
+console.log("practical");
+console.log(heapExample);
+console.log(heapTest);
---
+++ Day_no_5/nums_and_math.js
@@ -0,0 +1,35 @@
+// let score = 400;
+// console.log(score);
+
+// let balance = new Number(100);
+// console.log(balance);
+
+// // // To String and To Fixed
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// // // Precision 
+// const otherNumber = 125.890;
+// console.log(otherNumber.toPrecision(3))
+
+
+// // // To local String
+// const local = 1000000;
+// console.log(local.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++
+
+console.log(Math);
+
+// // absolute value
+console.log(Math.abs(-4));
+// // Rounde value
+console.log(Math.round(12.532));
+// // Floor Value
+console.log(Math.floor(12.232));
+// // Ceil Value
+console.log(Math.ceil(12.432));
+
+
