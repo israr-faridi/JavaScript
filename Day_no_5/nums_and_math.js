05e979effd83900178f6dde91fe6851461b7f33a
israr-faridi <vbmovielover465@gmail.com>
2024-03-20 12:55:10 +0500

    Array

--- Day_no_5/nums_and_math.js
+++ Day_no_5/nums_and_math.js
@@ -35,11 +35,15 @@
 
 // // Math Random
 
-console.log(Math.random());
-console.log((Math.random() * 10) + 1);
-console.log(Math.floor((Math.random() * 10) + 1));
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+let myDate = new Date()
+console.log(myDate.toString());
---
+++ Day_no_6/array.js
@@ -0,0 +1,34 @@
+// // Array number
+let arr = [1, 3, 5, 6, 7];
+
+// // Array in string
+let hero = ["spiderman", "ironman", "hulk"]
+
+// // Array all datatype
+let allType = [1, 4, 6, 54, "Kamil", "Usman", false]
+
+// // New Array creation method
+let newArr = new Array(44, 3, 55, 77, 22)
+
+
+// // Array length
+// console.log(newArr.length);
+
+// // Join Array we can change array in string with join arrray
+let joinArray = arr.join();
+
+// console.log(joinArray);
+// console.log(typeof(joinArray));
+
+
+// // Slice
+let sliceArray = arr.slice(1, 3)
+
+// console.log(sliceArray);
+// console.log(arr);
+
+// // splice
+let spliceArry = arr.splice(1, 3)
+
+// console.log(arr);
+// console.log(spliceArry);
