675b6e3472d4d45769e9131a0ea443863a33ba69
israr-faridi <vbmovielover465@gmail.com>
2024-03-27 14:21:11 +0500

    Array_Object_IN_Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,7 +29,7 @@ let result = sumTwoNum(5, 5)
 
 
 
-function loginMessage(username="same") {
+function loginMessage(username = "same") {
     // if (username === undefined) {
     //     console.log("Please enter a username");
     //     return
@@ -40,4 +40,40 @@ function loginMessage(username="same") {
     }
     return `${username} just logged in`
 }
-console.log(loginMessage());
+// console.log(loginMessage());
+
+
+
+// // price item calculation with rest 
+function itemPrice(...num1) {
+    return num1
+}
+// console.log(itemPrice(550,55,2,8,9,5));
+
+
+
+
+// //Object with function
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.price}`);
+}
+// let user = {
+//     name: "Israr",
+//     price: 700
+// }
+// handleObject(user)
+
+// handleObject({
+//     name: "Ali",
+//     price: 1500
+// })
+
+
+// // Function in Array 
+let myNewArray=[55,6,50,77,3,5];
+function returnSecondValuceOfArray(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValuceOfArray(myNewArray));
+console.log(returnSecondValuceOfArray([5,66,22,55,44,3,5]));
