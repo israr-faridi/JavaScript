7557968db943c08f444f068d85bb3e8cd123d919
israr-faridi <vbmovielover465@gmail.com>
2024-05-15 21:09:07 +0500

    forLoop

---
+++ 05_iterations/one.js
@@ -0,0 +1,72 @@
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (i===5) {
+//         console.log(`${i} is the best number`)
+//     }
+//      console.log(element)
+
+// }
+
+
+
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value is : ${i}`)
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`inner loop vaue is : ${j}, outer loop value is : ${i}`)
+        // console.log(i + " * " + j + " = " + i * j)
+    }
+}
+
+
+//// Arry in loop
+// let myArray=["Spiderman","Badman","Flash"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+// }
+
+
+
+
+
+
+
+// // BREAK IN LOOP
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log("Detected 5")
+//         break;
+//     }
+//     console.log(`Value of index is : ${index}`)
+// }
+
+
+
+
+
+
+
+
+
+// // CONTINUE IN LOOP
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log("Detected 5")
+//         continue;
+//     }
+//     console.log(`Value of index is : ${index}`)
+// }
+
+
+
+
+
+
+
+
+
