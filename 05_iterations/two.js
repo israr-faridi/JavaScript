1b9791c3fbae9f3b1313acf51085fde7f977680d
israr-faridi <vbmovielover465@gmail.com>
2024-05-15 21:10:19 +0500

    while_and_doWhileLoop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+let index = 0;
+while (index <= 10) {
+    // console.log(`Value of index is ${index} `);
+    index = index + 2;
+
+}
+
+
+let myArr = ["spiderman", "badman", "superman", "flash"];
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`Marvel Hero = ${myArr[arr]}`)
+    arr = arr + 1
+}
+
+
+
+
+// ****************************** DO WHILE LOOP ***************************************
+let score=1
+do {
+    console.log(`Sorce is = ${score}`)
+    score++;
+} while (score<=10);
