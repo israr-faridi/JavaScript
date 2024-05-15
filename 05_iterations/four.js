b8b3c961e6db22ba464a08e51ab2b71e6886e227
israr-faridi <vbmovielover465@gmail.com>
2024-05-15 21:11:51 +0500

    for_in_loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+//************************************* FOR IN LOOP ****************************************** 
+let myObject = {
+    js: "JavaScript",
+    html: "Hyper Text Mark Language",
+    css: "Castading Styling Sheet"
+}
+
+for (const key in myObject) {
+    // console.log(`key is : ${key} and value is : ${myObject[key]}`)
+}
+
+
+
+// arry
+let programming=['js','python','cpp','java']
+for (const key in programming) {
+console.log(programming[key])
+}
